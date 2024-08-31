f978f523cb363219d75e2336355b0ed3737213d3
Imanwel <nuel5572@gmail.com>
2024-09-01 00:27:17 +0100

    update

--- content/js/form.js
+++ content/js/form.js
@@ -1,6 +1,6 @@
 const form = document.querySelector("form");
 const inputValue = document.querySelectorAll(".formInput");
-
+const signInForm = document.querySelector(".signIn");
 // const errorMessage = document.querySelector(".empty");
 const button = document.querySelector(".btn");
 
@@ -15,7 +15,20 @@ button.addEventListener("mouseleave", () => {
 //   timeOut(3000);
 // });
 
+const validateSuccess = (val, errorMessage) => {
+  errorMessage.style.visibility = "hidden";
+  val.firstElementChild.classList.remove("invalid");
+  val.firstElementChild.classList.add("valid");
+};
+const validateError = (val, cautionMessage, errorMessage) => {
+  errorMessage.style.visibility = "visible";
+  errorMessage.textContent = cautionMessage;
+  val.firstElementChild.classList.remove("valid");
+  val.firstElementChild.classList.add("invalid");
+};
+
 form.addEventListener("submit", runSubmit);
+
 function runSubmit(e) {
   e.preventDefault();
   let eachInput = Array.from(form.children);
@@ -92,7 +105,11 @@ function runSubmit(e) {
     validated.confirmPassword !== ""
   ) {
     saveInputsValue();
-    timeOut(3000);
+    timeOut(
+      3000,
+      "Account created successfully",
+      "http://127.0.0.1:5502/signIn.html"
+    );
     // console.log("good");
   } else {
     console.log("not saved to storage");
@@ -110,22 +127,88 @@ function runSubmit(e) {
   }
 }
 
-function timeOut(time) {
+signInForm.addEventListener("submit", runLogin);
+
+let validateSignIn = {
+  email: "",
+  password: "",
+};
+
+function runLogin(e) {
+  e.preventDefault();
+
+  let allInputs = document.querySelectorAll(".formInput");
+  let inputValues = Array.from(allInputs);
+  let [val1, val2] = inputValues;
+
+  let inputFields = document.querySelectorAll(".fields");
+  let theInputs = Array.from(inputFields);
+
+  theInputs.forEach((item) => {
+    if (item.firstElementChild.classList.contains("signInEmail")) {
+      if (item.firstElementChild.value === "") {
+        validateError(item, "invalid email", item.lastElementChild);
+        validateSignIn.email = "";
+      } else {
+        validateSuccess(item, item.lastElementChild);
+        validateSignIn.email = val1.value;
+        // signIn();
+      }
+    } else if (item.firstElementChild.classList.contains("signInPassword")) {
+      if (item.firstElementChild.value === "") {
+        validateError(item, "incorrect password", item.lastElementChild);
+        validateSignIn.password = "";
+      } else {
+        validateSuccess(item, item.lastElementChild);
+        validateSignIn.password = val2.value;
+        // signIn();
+      }
+    }
+  });
+
+  if (validateSignIn.email !== "" && validateSignIn.password !== "") {
+    console.log(validateSignIn);
+    signIn();
+  }
+  console.log(val1.value);
+}
+
+// function timeOut(time) {
+//   document.querySelector(".account-creation").style.display = "block";
+//   setTimeout(() => {
+//     document.querySelector(".account-creation").style.display = "none";
+//     window.location.href = "http://127.0.0.1:5502/signIn.html";
+//   }, time);
+// }
+
+function signIn() {
+  let signInDetails;
+  if (localStorage.getItem("signInDetails") === null) {
+    signInDetails = [];
+  } else {
+    signInDetails = JSON.parse(localStorage.getItem("signInDetails"));
+  }
+  signInDetails.push(validateSignIn);
+  localStorage.setItem("signInDetails", JSON.stringify(signInDetails));
+
+  if (
+    localStorage.getItem("details") === localStorage.getItem("signInDetails")
+  ) {
+    console.log("you're good to go");
+    timeOut(3000, "login successful", "http://127.0.0.1:5502/");
+  } else {
+    console.log("there's error in your work");
+  }
+}
+
+function timeOut(time, message, location) {
   document.querySelector(".account-creation").style.display = "block";
+  document.querySelector(".account-creation").textContent = message;
   setTimeout(() => {
     document.querySelector(".account-creation").style.display = "none";
-    location.href = "http://127.0.0.1:5502/signIn.html";
+    // window.location.href = "http://127.0.0.1:5502/";
+    window.location.href = location;
   }, time);
 }
 
-const validateSuccess = (val, errorMessage) => {
-  errorMessage.style.visibility = "hidden";
-  val.firstElementChild.classList.remove("invalid");
-  val.firstElementChild.classList.add("valid");
-};
-const validateError = (val, cautionMessage, errorMessage) => {
-  errorMessage.style.visibility = "visible";
-  errorMessage.textContent = cautionMessage;
-  val.firstElementChild.classList.remove("valid");
-  val.firstElementChild.classList.add("invalid");
-};
+console.log(localStorage.getItem("details"));
